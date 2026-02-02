559d6b5f4eb20593d988f148af544eb8fc887351
MD TASNIM <mdtasnim0503@gmail.com>
2026-02-03 00:52:04 +0530

    modify sidebar of admin dashboard

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,12 @@
 import { KeyboardArrowRight } from "@mui/icons-material";
 import {
+  Avatar,
   Box,
+  DialogContent,
+  DialogTitle,
   Divider,
+  Drawer,
+  IconButton,
   List,
   ListItem,
   ListItemButton,
@@ -9,6 +14,7 @@ import {
   ListItemDecorator,
   Typography,
 } from "@mui/joy";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import GroupIcon from "@mui/icons-material/Group";
 import ShowChartIcon from "@mui/icons-material/ShowChart";
@@ -17,7 +23,11 @@ import FormatListNumberedIcon from "@mui/icons-material/FormatListNumbered";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import LogoutIcon from "@mui/icons-material/Logout";
 import { NavLink, useNavigate } from "react-router-dom";
+import { useMediaQuery } from "@mui/material";
+import { useState } from "react";
 export default function SideBar() {
+  const isSmall = useMediaQuery("(max-width:700px");
+  const [sideBarOpen, setSideBarOpen] = useState(true);
   const sideBarLinks = [
     {
       icon: <DashboardIcon />,
@@ -42,42 +52,68 @@ export default function SideBar() {
 
   return (
     <Box>
-      <List
-        sx={{ width: "260px", height: "100vh", border: "1px solid #00000033" }}
+      <Drawer
+        open={true}
+        sx={{
+          "& .css-16k4q1o-JoyDrawer-content": {
+            width: sideBarOpen ? "300px" : "50px",
+            transition: "width 0.5s ease",
+            overflowX: "hidden",
+          },
+        }}
       >
-        <Box sx={{ display: "grid", gridTemplateColumns: "1fr 35px", py: 2 }}>
-          <Typography sx={{ pl: 2, color: "#171a1cb8" }} level="title-lg">
-            Md Tasnim
-          </Typography>
-          <KeyboardArrowRight />
-        </Box>
-        <Divider />
-
-        <ListItem sx={{ display: "flex", flexDirection: "column", mt: 2 }}>
-          {sideBarLinks.map((item) => (
-            <ListItemButton
-              component={NavLink}
-              to={item.href}
-              onClick={() => handlePage(item.href)}
-              key={item.label}
-              sx={{
-                "&.active": {
-                  backgroundColor: "#547792",
-                  color: "#fff",
-                },
-                "&.active:hover": {
-                  backgroundColor: "#3e5a70",
-                  color: "#fff",
-                },
-              }}
+        <DialogTitle>
+          <Box
+            sx={{
+              display: "grid",
+              gridTemplateColumns: "1fr 26px",
+              justifyContent: "space-between",
+              width: "100%",
+            }}
+          >
+            <Typography
+              sx={{ color: "#171a1cb8" }}
+              level="title-lg"
+              startDecorator={<Avatar variant="solid" />}
             >
-              <ListItemDecorator>{item.icon}</ListItemDecorator>
-              <ListItemContent>{item.label}</ListItemContent>
-              <KeyboardArrowRight sx={{ fontSize: "1.2rem" }} />
-            </ListItemButton>
-          ))}
-        </ListItem>
-      </List>
+              Md Tasnim
+            </Typography>
+            <IconButton onClick={() => setSideBarOpen(!sideBarOpen)}>
+              <KeyboardArrowLeftIcon />
+            </IconButton>
+          </Box>
+        </DialogTitle>
+        <DialogContent>
+          <Divider />
+          <List>
+            <ListItem sx={{ display: "flex", flexDirection: "column", mt: 1 }}>
+              {sideBarLinks.map((item) => (
+                <ListItemButton
+                  component={NavLink}
+                  to={item.href}
+                  onClick={() => handlePage(item.href)}
+                  key={item.label}
+                  sx={{
+                    "&.active": {
+                      backgroundColor: "#547792",
+                      color: "#fff",
+                    },
+                    "&.active:hover": {
+                      backgroundColor: "#3e5a70",
+                      color: "#fff",
+                    },
+                    padding: ".7rem",
+                  }}
+                >
+                  <ListItemDecorator>{item.icon}</ListItemDecorator>
+                  <ListItemContent>{item.label}</ListItemContent>
+                  <KeyboardArrowRight sx={{ fontSize: "1.2rem" }} />
+                </ListItemButton>
+              ))}
+            </ListItem>
+          </List>
+        </DialogContent>
+      </Drawer>
     </Box>
   );
 }
